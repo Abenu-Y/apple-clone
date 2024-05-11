9ab7e33c4314561ce9e44f2da729d07fa3f3a4ca
Abenezer Y <abeneyoseph@gmail.com>
2024-05-12 01:47:32 +0300

    firebase auto deploy

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAc6xHZf_xlZVYGgLZMaQUfAligLSPsgA4",
+  authDomain: "test-apple-325d7.firebaseapp.com",
+  projectId: "test-apple-325d7",
+  storageBucket: "test-apple-325d7.appspot.com",
+  messagingSenderId: "476290381767",
+  appId: "1:476290381767:web:6f54f5ef41b6767980cbe2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
