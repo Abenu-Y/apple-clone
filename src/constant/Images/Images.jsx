01e9ddb468fa857c0ffb801464d86bcbe6d43ffe
Abenezer Y <abeneyoseph@gmail.com>
2024-05-12 00:17:29 +0300

    test ... deploy on github using firebase

---
+++ src/constant/Images/Images.jsx
@@ -0,0 +1,39 @@
+
+
+import logo from '../../assets/Resource/Images/Icons/logo-sm.png'
+import search from '../../assets/Resource/Images/Icons/search-icon-sm.png'
+import cart from '../../assets/Resource/Images/Icons/cart-sm.png'
+import watchLogo from '../../assets/Resource/Images/Icons/watchLogo.png'
+import vision from '../../assets/Resource/Images/Icons/visionProLogo.png'
+import trade from '../../assets/Resource/Images/Icons/tradeInLogo.png'
+import card from '../../assets/Resource/Images/Icons/card.png'
+
+
+import comedy from '../../assets/Resource/Rotated Images/Carousel/Comedy.jpg'
+
+import documentary from '../../assets/Resource/Rotated Images/Carousel/Documentary.jpg'
+
+import drama from '../../assets/Resource/Rotated Images/Carousel/Drama.jpg'
+
+import livegame from '../../assets/Resource/Rotated Images/Carousel/Live MLB games.jpg'
+
+import mls from '../../assets/Resource/Rotated Images/Carousel/MLS.jpg'
+import fiction from '../../assets/Resource/Rotated Images/Carousel/Sci-Fi.jpg'
+import thriller from '../../assets/Resource/Rotated Images/Carousel/Thriller.jpg'
+
+
+
+import nba from '../../assets/Resource/Rotated Images/rotated pic/NBA 2K24 Arcade Edition.jpg'
+import vibes from '../../assets/Resource/Rotated Images/rotated pic/Vibes.jpg'
+import island from '../../assets/Resource/Rotated Images/rotated pic/Hello Kitty Island Adventures.jpg'
+import hits from "../../assets/Resource/Rotated Images/rotated pic/Today's Hits.jpg"
+import yoga from '../../assets/Resource/Rotated Images/rotated pic/Yoga with Molly.jpg'
+import glutes from '../../assets/Resource/Rotated Images/rotated pic/Get Your Glutes in Great Shape.jpg'
+
+
+const Images = {
+   logo,search,cart,watchLogo,vision,trade,card,comedy,documentary,drama,livegame,mls,fiction,thriller,nba,vibes,island,hits,yoga,glutes
+}
+
+
+export default Images
